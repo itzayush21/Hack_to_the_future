9af950351ebd485f8baf1c46f92abb3ac3ec250f
suprit-singh <supritsingh2001@gmail.com>
2024-07-21 01:46:28 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,8 @@
+let abt = document.getElementById("abt");
+abt.addEventListener("click",()=>{
+    document.querySelector(".para").innerHTML = "<b>Welcome to our Personalized Health Checkup platform!</b>  We're dedicated to empowering individuals with the knowledge and tools they need to lead healthier lives. Our comprehensive health survey collects detailed information about your lifestyle, diet, and medical history to provide you with tailored feedback and actionable wellness recommendations. At our core, we believe that personalized health insights are the key to unlocking your best self. Join us on a journey towards better health and well-being, one step at a time."
+})
+let cnt = document.getElementById("cnt");
+cnt.addEventListener("click",()=>{
+    document.querySelector(".para").innerHTML = "Team name: <b>YODDHA</b> <br>Team members: <li>Ayush Kumar Singh   <b>Email id:</b> itzayush21@gmail.com </li><li>Harsh Raj <b>Email id:</b> harsh.hitk26@gmail.com</li><li>Suprit Kumar <b>Email id:</b> supritkumar1112@gmail.com</li><li>Anmol Raj  <b>Email id:</b> anmol.raj.ds26@heritageit.edu.in</li>"
+})
